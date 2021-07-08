6cc5e6501d7c8f636549940e0ad87cbe16ba2cda
cyran <gs.gouin@gmail.com>
2021-07-09 00:26:13 +0200

    table mis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
 import {TableModule} from "primeng/table";
 import {CarService} from "./home/car.service";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
@@ -12,7 +11,6 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
